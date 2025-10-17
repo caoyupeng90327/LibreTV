2928f35300535a4b3e36051e19c3262c0ef87001
caoyupeng90327 <40814774+caoyupeng90327@users.noreply.github.com>
2025-10-17 16:54:11 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -119,97 +119,128 @@ const API_SITES = {
         api: 'http://vod.9e03.com/lvdou_api.php/v1.vod'
     },
     mjf : {
-        name: '美剧范','url':'http://ttzmz.net/api.php/v1.vod'
+        name: '美剧范',
+        api:'http://ttzmz.net/api.php/v1.vod'
     },
     mjc : {
-        name: '美剧虫','url':'https://meijuchong.com/api.php/v1.vod'
+        name: '美剧虫',
+        api:'https://meijuchong.com/api.php/v1.vod'
     },
     ydyy : {
-        name: '益达影院','url':'http://luobu.yss6080.com/mogai_api.php/v1.vod'
+        name: '益达影院',
+        api:'http://luobu.yss6080.com/mogai_api.php/v1.vod'
     },
     dmys : {
-        name: '大猫影视','url':'http://app.ishen520.com/api.php/v1.vod'
+        name: '大猫影视',
+        api:'http://app.ishen520.com/api.php/v1.vod'
     },
     yyys : {
-        name: '渔渔影视','url':'http://luobo.yugenye.site/api.php/v1.vod'
+        name: '渔渔影视',
+        api:'http://luobo.yugenye.site/api.php/v1.vod'
     },
     tzw : {
-        name: '兔子窝','url':'http://cj.huimaojia.com:12345/mogai_api.php/v1.vod'
+        name: '兔子窝',
+        api:'http://cj.huimaojia.com:12345/mogai_api.php/v1.vod'
     },
     tpys : {
-        name: '躺平影视','url':'http://www.lltpys.com/api.php/app/'
+        name: '躺平影视',
+        api:'http://www.lltpys.com/api.php/app/'
     },
     k365 :{
-        name: '看365','url':'https://www.kan365.xyz/api.php/app/'
+        name: '看365',
+        api:'https://www.kan365.xyz/api.php/app/'
     },
     bc  : {
-        name: '北川','url':'https://www.bcwzg.com/api.php/app/'
+        name: '北川',
+        api:'https://www.bcwzg.com/api.php/app/'
     },
     ztys : {
-        name: '钟特影视','url':'https://app.zteys.com/api.php/v1.vod'
+        name: '钟特影视',
+        api:'https://app.zteys.com/api.php/v1.vod'
     },
     tkv2 : {
-        name: '天空V2','url':'https://www.tkys.tv/xgapp.php/v2/'
+        name: '天空V2',
+        api:'https://www.tkys.tv/xgapp.php/v2/'
     },
     sryy : {
-        name: '三日影院','url':'https://www.3ri.net/api.php/v1.vod'
+        name: '三日影院',
+        api:'https://www.3ri.net/api.php/v1.vod'
     },
     slyy : {
-        name: '双鹿影院','url':'http://cr.slidc.top/mubai_api.php/m2.vod'
+        name: '双鹿影院',
+        api:'http://cr.slidc.top/mubai_api.php/m2.vod'
     },
     fxsj : {
-        name: '粉象视界','url':'http://42.157.129.15:34444/lvdou_api.php/v1.vod'
+        name: '粉象视界',
+        api:'http://42.157.129.15:34444/lvdou_api.php/v1.vod'
     },
     lmys : {
-        name: '懒猫影视','url':'https://lanmao.lanmaoymw.cn/api.php/v1.vod'
+        name: '懒猫影视',
+        api:'https://lanmao.lanmaoymw.cn/api.php/v1.vod'
     },
     al : {
-        name: '阿里','url':'http://aliys.cn:90/api.php/v1.vod'
+        name: '阿里',
+        api:'http://aliys.cn:90/api.php/v1.vod'
     },
     qc : {
-        name: 'QC','url':'https://www.qcsvip.com/mogai_api.php/v1.vod'
+        name: 'QC',
+        api:'https://www.qcsvip.com/mogai_api.php/v1.vod'
     },
     hh : {
-        name: '黄河','url':'http://i.ledu8.cn/api.php/v1.vod'
+        name: '黄河',
+        api:'http://i.ledu8.cn/api.php/v1.vod'
     },
     jikusp :{
-        name: '极酷视频','url':'https://jiku.vip/mogai_api.php/v1.vod'
+        name: '极酷视频',
+        api:'https://jiku.vip/mogai_api.php/v1.vod'
     },
     juzi : {
-        name: '橘子','url':'http://jz.juzidy.vip/mogai_api.php/v1.vod'
+        name: '橘子',
+        api:'http://jz.juzidy.vip/mogai_api.php/v1.vod'
     },
     qifeizx : {
-        name: '奇飞专线','url':'http://mkk.gotka.top/api.php/v1.vod'
+        name: '奇飞专线',
+        api:'http://mkk.gotka.top/api.php/v1.vod'
     },
     tkyinshi : {
-        name: '天空影视','url':'https://tkys.tv/xgapp.php/v1/'
+        name: '天空影视',
+        api:'https://tkys.tv/xgapp.php/v1/'
     },
     duanyou2ys :{
-        name: '段友2影视','url':'https://shangjihuoke.com/api.php/tv.vod'
+        name: '段友2影视',
+        api:'https://shangjihuoke.com/api.php/tv.vod'
     },
     nuanguangys : {
-        name: '暖光影视','url':'https://app.bl210.com/api.php/v1.vod'
+        name: '暖光影视',
+        api:'https://app.bl210.com/api.php/v1.vod'
     },
     tianchengyingshi : {
-        name: '天诚影视','url':'http://tcspvip.cn/api.php/v1.vod'
+        name: '天诚影视',
+        api:'http://tcspvip.cn/api.php/v1.vod'
     },
     aiteys : {
-        name: '艾特影视','url':'https://www.aitee.cc/api.php/v1.vod'
+        name: '艾特影视',
+        api:'https://www.aitee.cc/api.php/v1.vod'
     },
     duanyouys1 :{
-        name: '段友影视1','url':'http://js.66app.me/api.php/app/'
+        name: '段友影视1',
+        api:'http://js.66app.me/api.php/app/'
     },
     bilinys :{
-        name: '比邻影视','url':'http://0hzy.cn:9990/mogai_api.php/v1.vod'
+        name: '比邻影视',
+        api:'http://0hzy.cn:9990/mogai_api.php/v1.vod'
     },
     zuijudaren : {
-        name: '追剧达人','url':'https://www.zhuijudaren.com/mogai_api.php/v1.vod'
+        name: '追剧达人',
+        api:'https://www.zhuijudaren.com/mogai_api.php/v1.vod'
     },
     yuanxiang69 : {
-        name: '69院线','url':'http://app.269w.com/api.php/v1.vod'
+        name: '69院线',
+        api:'http://app.269w.com/api.php/v1.vod'
     },
     chanyi :{
-        name: '创艺','url':'http://www.30dian.cn/api.php/v1.vod'
+        name: '创艺',
+        api:'http://www.30dian.cn/api.php/v1.vod'
     }
     // testSource: {
     //     api: 'https://www.example.com/api.php/provide/vod',
